36b7b1f75894e4b349dc6899d088a23c29f6bb44
alvaro garcia <derecerca@gmail.com>
2015-07-22 20:00:22 +0200

    refactor: DRY - extract sut initialization to @before

--- src/test/java/TreeShould.java
+++ src/test/java/TreeShould.java
@@ -1,4 +1,5 @@
 import bst.Tree;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,19 +9,18 @@ public class TreeShould {
 
 	private Tree sut;
 
+	@Before
+	public void setUp () {
+		sut = Tree.from(1);
+	}
+
 	@Test
 	public void store_an_element () {
-
-		sut = Tree.from(1);
-
 		assertThat(sut.contains(1), is(true));
 	}
 
 	@Test
 	public void not_contain_a_previously_added_element () {
-
-		sut = Tree.from(1);
-
 		assertThat(sut.contains(2), is(false));
 	}
 }
