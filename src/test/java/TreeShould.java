37a74dd9b6cf716085f341ec1be8d421df0a3a23
alvaro garcia <derecerca@gmail.com>
2015-07-22 19:56:35 +0200

    red: tree should contain a single element

---
+++ src/test/java/TreeShould.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TreeShould {
+
+	private Tree sut;
+
+	@Test
+	public void store_an_element () {
+
+		sut = Tree.from(1);
+
+		assertThat(sut.contains(1), is(true));
+	}
+}
