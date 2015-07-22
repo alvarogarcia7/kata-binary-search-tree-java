e3e6c8beb88c1e9f2519f0efc07243a99d0c006a
alvaro garcia <derecerca@gmail.com>
2015-07-22 20:22:57 +0200

    green

--- src/main/java/bst/Tree.java
+++ src/main/java/bst/Tree.java
@@ -6,10 +6,12 @@ public class Tree {
 	private final int value;
 	private Optional<Integer> other;
 	private Optional<Integer> other2;
+	private Optional<Integer> other3;
 
 	private Tree (final int value) {
 		other = Optional.empty();
 		other2 = Optional.empty();
+		other3 = Optional.empty();
 		this.value = value;
 	}
 
@@ -20,12 +22,17 @@ public class Tree {
 	public boolean contains (final int value) {
 		return this.value == value
 				|| other.isPresent() && other.get() == value
-				|| other2.isPresent() && other2.get() == value;
+				|| other2.isPresent() && other2.get() == value
+				|| other3.isPresent() && other3.get() == value;
 	}
 
 	public void add (final int value) {
 		if (other.isPresent()) {
-			other2 = Optional.of(value);
+			if (other2.isPresent()) {
+				other3 = Optional.of(value);
+			} else {
+				other2 = Optional.of(value);
+			}
 		} else {
 			other = Optional.of(value);
 		}
