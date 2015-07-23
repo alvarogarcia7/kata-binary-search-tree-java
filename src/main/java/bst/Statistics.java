c241341f81e90011f3db797e4721e8226338124a
alvaro garcia <derecerca@gmail.com>
2015-07-23 20:50:26 +0200

    refactor: extract method for calculating log2

--- src/main/java/bst/Statistics.java
+++ src/main/java/bst/Statistics.java
@@ -13,12 +13,16 @@ public class Statistics {
 	private int comparisons = 0;
 
 	public boolean isLog2N (final int n) {
-		final double log = Math.log(n) / Math.log(2);
+		final double log = log2(n);
 		final int upperLimit = (int) Math.ceil(log);
 		return comparisons < upperLimit;
 	}
 
 	public void comparison () {
 		this.comparisons++;
+	}
+
+	private double log2 (final int n) {
+		return Math.log(n) / Math.log(2);
 	}
 }
