fd50ee58981385a558c4ad62c8d32a33bc675956
alvaro garcia <derecerca@gmail.com>
2015-07-23 20:52:02 +0200

    refactor: for readability, the NONE should go at the end

--- src/main/java/bst/Statistics.java
+++ src/main/java/bst/Statistics.java
@@ -1,17 +1,7 @@
 package bst;
 
 public class Statistics {
-	public static final Statistics NONE = new Statistics(){
-		@Override
-		public void comparison () {}
-
-		@Override
-		public boolean isLog2N (final int n) {
-			throw new UnsupportedOperationException();
-		}
-	};
 	private int comparisons = 0;
-
 	public boolean isLog2N (final int n) {
 		return comparisons < log2Ceiled(n);
 	}
@@ -24,4 +14,14 @@ public class Statistics {
 		final double log = Math.log(n) / Math.log(2);
 		return (int) Math.ceil(log);
 	}
+
+	public static final Statistics NONE = new Statistics(){
+		@Override
+		public void comparison () {}
+
+		@Override
+		public boolean isLog2N (final int n) {
+			throw new UnsupportedOperationException();
+		}
+	};
 }
